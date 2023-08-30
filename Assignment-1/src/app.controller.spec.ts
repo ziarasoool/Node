f4ee36da568f6c8dcd94d20e737f45440af49cae
ziarasoool <39952126+ziarasoool@users.noreply.github.com>
2023-08-30 18:27:54 +0500

    Update app.controller.spec.ts

--- Assignment-1/src/app.controller.spec.ts
+++ Assignment-1/src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "Hello Zia!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
